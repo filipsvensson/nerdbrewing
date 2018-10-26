03026c4f2374a0e5b6d04f4e024bde7ce4433567
Filip Svensson <filip.svensson@jayway.com>
2018-10-26 11:23:05 +0200

    recipe grid test on new staging branch

--- src/RecipeList/RecipeList.jsx
+++ src/RecipeList/RecipeList.jsx
@@ -15,22 +15,42 @@ const query = `{
 class RecipeList extends Component {
   render() {
     return (
-      <div className="recipie-list">
-        <Fetch query={query}>
-          {res =>
-            res.recipes.map(({ name, thumbnail: { url } }) => {
-              return (
-                <div className="recipie-list-item">
-                  <img className="recipe-thumbnail" src={url} />
-                  <h1 className="recipe-name">{name}</h1>
-                </div>
-              );
-            })
-          }
-        </Fetch>
+      <div className="recipes">
+        <div className="recipie-grid">
+            <div className="recipie-grid-item">
+              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+              <div className="recipe-title">Volatile</div>
+            </div>
+            <div className="recipie-grid-item">
+              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+              <div className="recipe-title">Volatile</div>
+            </div>
+            <div className="recipie-grid-item">
+              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+              <div className="recipe-title">Volatile</div>
+            </div>
+            <div className="recipie-grid-item">
+              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+              <div className="recipe-title">Volatile</div>
+            </div>
+        </div>
       </div>
+
     );
   }
 }
+// <h1 className="recipe-name">{name}</h1>
 
 export default RecipeList;
+
+// <Fetch query={query}>
+//   {res =>
+//     res.recipes.map(({ name, thumbnail: { url } }) => {
+//       return (
+//         <div className="recipie-grid-item">
+//           <img className="recipe-thumbnail" src={url} />
+//         </div>
+//       );
+//     })
+//   }
+// </Fetch>
