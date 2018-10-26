307b111cf51dabad2065eab59504b30e7c7cdd46
Filip Svensson <filip.svensson@jayway.com>
2018-10-26 13:54:39 +0200

    tagbox test

--- src/RecipeList/RecipeList.jsx
+++ src/RecipeList/RecipeList.jsx
@@ -17,30 +17,35 @@ class RecipeList extends Component {
     return (
       <div className="recipes">
         <div className="recipie-grid">
-            <div className="recipie-grid-item">
-              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
-              <div className="recipe-title">Volatile</div>
-            </div>
-            <div className="recipie-grid-item">
-              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
-              <div className="recipe-title">Volatile</div>
-            </div>
-            <div className="recipie-grid-item">
-              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
-              <div className="recipe-title">Volatile</div>
-            </div>
-            <div className="recipie-grid-item">
-              <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
-              <div className="recipe-title">Volatile</div>
-            </div>
+          <div className="taggbox-container">
+            <script defer src="//7499608.com/app/js/embed.min.js" type="text/javascript">
+            </script><div className="taggbox-socialwall" data-wall-id="nerdbrewing">
+          </div>
+        </div>
         </div>
       </div>
 
     );
   }
 }
-// <h1 className="recipe-name">{name}</h1>
+// <h1 classNameName="recipe-name">{name}</h1>
 
+// <div className="recipie-grid-item">
+//   <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+//   <div className="recipe-title">Volatile</div>
+// </div>
+// <div className="recipie-grid-item">
+//   <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+//   <div className="recipe-title">Volatile</div>
+// </div>
+// <div className="recipie-grid-item">
+//   <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+//   <div className="recipe-title">Volatile</div>
+// </div>
+// <div className="recipie-grid-item">
+//   <img className="recipe-thumbnail" src="https://media.graphcms.com//vBotYcYrSNmrxHUrraDK" />
+//   <div className="recipe-title">Volatile</div>
+// </div>
 export default RecipeList;
 
 // <Fetch query={query}>
