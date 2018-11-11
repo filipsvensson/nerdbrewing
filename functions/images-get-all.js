38b9c4327a6ce2dcf1e448713467985d23b97aed
Filip Svensson <filip.svensson@jayway.com>
2018-11-11 19:33:50 +0100

    test

--- functions/images-get-all.js
+++ functions/images-get-all.js
@@ -11,35 +11,31 @@ const ImageModel = mongoose.model('Image', ImageSchema);
 
 
 /* export our lambda function as named "handler" export */
-exports.handler = async (event, context, callback) => {
-
+exports.handler = async (event, context) => {
   console.log(chalk.green('Function `images-get-all` invoked'))
   console.log('process.env.MONGODB_URI', process.env.MONGODB_URI);
 
   if(!process.env.MONGODB_URI) {
     console.log(chalk.yellow('Required MONGODB_URI enviroment variable not found.'))
   }
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify([])
-  })
+
   await mongoose.connect(process.env.MONGODB_URI).catch((err) => {
     console.log(chalk.red('mongoose error'), err)
-    return callback(null, {
+    return {
       statusCode: 500,
       body: JSON.stringify(err)
-    })
+    }
   });
   console.log(chalk.green('mongoose connection success'))
 
   try {
     const res = await ImageModel.find({}).sort('-date').limit(10).exec();
     console.log(chalk.green('mongoose success result: '), res)
-    callback(null, {
+
+    return {
       statusCode: 200,
       body: JSON.stringify(res)
-    })
-
+    }
   } catch (err) {
     console.log(err);
   }
