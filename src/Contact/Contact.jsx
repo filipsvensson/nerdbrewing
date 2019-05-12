2639194a055e981f485f3b276ecf0c0a8af4a676
Filip Svensson <filip.svensson@jayway.com>
2019-05-12 15:12:32 +0200

    test of stateful contact form

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
+import ContactForm from './ContactForm';
 import SocialLinks from '../SocialLinks/SocialLinks';
 
 import './Contact.css';
 
 const Contact = () => (
   <address className="contact">
-    <form name="contact" method="post">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label htmlFor="name">
-          Your Name:
-          <input type="text" name="name" id="name" />
-        </label>
-      </p>
-      <p>
-        <label htmlFor="email">
-          Your Email:
-          <input type="email" name="email" id="email" />
-        </label>
-      </p>
-      <p>
-        <label htmlFor="message">
-          Message:
-          <textarea name="message" id="massage" />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <ContactForm />
 
     <div className="contact-info">
       <div>info@nerdbrewing.se</div>
